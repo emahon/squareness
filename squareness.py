f40940c5767dc69417b9dbbdc1eee0ae703ad954
E <esmahon11@gmail.com>
2021-08-29 10:00:09 -0600

    Adding biggest factor

--- squareness.py
+++ squareness.py
@@ -1,6 +1,4 @@
 import math
-import numpy
-import pandas
 
 def determine_if_square(root):
     if (math.floor(root) == root):
@@ -25,23 +23,27 @@ def squarest_factor(n, root):
 
 if __name__ == '__main__':
     squareness_list = [1, 1] # squareness of 0 and 1 (special cases)
+    squarest_factor_list = ["0", "1"]
     primes_list = [2] # list of primes we've found so far
     rename_me = 1000000
     for n in range(2, rename_me):
         root = math.sqrt(n)
         if (determine_if_square(root)):
             squareness_list.append(1)
+            squarest_factor_list.append("square")
             continue
 
         if (determine_if_prime(n, root)):
             if (n != 2):
                 primes_list.append(n)
             squareness_list.append(1.0/root)
+            squarest_factor_list.append("prime")
             continue
 
         # so this is a non-square composite number
         i = squarest_factor(n, root)
         squareness_list.append(i/root)
+        squarest_factor_list.append(str(i))
     for index, val in enumerate(squareness_list):
-        print(str(index) + " " + str(val))
-    print(primes_list)
+        print(str(index) + " " + str(val) + " " + squarest_factor_list[index])
+    #print(primes_list)
